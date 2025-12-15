2c67e9ab84b6302b111d8ec15431811460e6302e
karthikkorrayi <karthikkorrayii@gmail.com>
2025-12-15 16:23:11 +0530

    uupdate

---
+++ budget-tracker/src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyBNGnXXho0kHwmEUTFJKtM5rMeNzLckWB8",
+    authDomain: "mybudgetapp-54c43.firebaseapp.com",
+    projectId: "mybudgetapp-54c43",
+    storageBucket: "mybudgetapp-54c43.firebasestorage.app",
+    messagingSenderId: "231457872788",
+    appId: "1:231457872788:web:4fabec01905c59479a0d81"
+  }
+};
