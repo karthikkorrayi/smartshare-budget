3c65484bacf43e19e94ab6f05d337f94c06bb3f8
Karthik Korrayi <78419475+karthikkorrayi@users.noreply.github.com>
2025-12-31 12:43:45 +0530

    Updated app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,19 +6,19 @@ import { PinLockComponent } from './security/pin-lock/pin-lock.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PinLockComponent],
+  imports: [RouterOutlet],
   template: `
   <header class="top-nav">
     <div class="nav-left">
       <span class="app-title">Personal Budget Insights</span>
     </div>
   </header>
-  <!-- <router-outlet></router-outlet>  -->
-  <app-pin-lock></app-pin-lock>
+  <router-outlet></router-outlet>
+  <!-- <app-pin-lock></app-pin-lock>
 
    @if (!pinService.isLocked) {
     <router-outlet></router-outlet>
-  }
+  } -->
   `,
   styleUrl:'./app.component.scss'
 })
